3f411b7b294ef05391fac8eb99546374d6caff79
Fabian Castillo <fabianboc960@hotmail.com>
2022-08-11 21:46:09 -0500

    Material Angular: Sidenav inicial

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule
+  ]
+})
+export class MaterialModule { }
