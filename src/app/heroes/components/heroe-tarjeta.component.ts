9e6132541bdebeb0987021f51c664cf1eee088d8
Fabian Castillo <fabianboc960@hotmail.com>
2022-08-14 20:28:00 -0500

    Tarea: Centralización del componente Card-Heroe

---
+++ src/app/heroes/components/heroe-tarjeta.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from "@angular/core";
+import { Heroe } from "../interfaces/heroes.interface";
+
+@Component({
+    selector: 'app-heroe-tarjeta',
+    templateUrl: 'heroe-tarjeta.component.html',
+    styles: []
+})
+export class HeroeTarjetaComponent {
+    @Input() public heroe!: Heroe;
+}
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -9,6 +9,7 @@ import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta.component';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
 
@@ -19,6 +20,7 @@ import { HeroesRoutingModule } from './heroes-routing.module';
     AgregarComponent,
     BuscarComponent,
     HeroeComponent,
+    HeroeTarjetaComponent,
     HomeComponent,
     ListadoComponent
   ],
