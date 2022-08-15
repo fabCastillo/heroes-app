6e41b23fc2e3c5d8ee7eb25d3456e0096565cf43
Fabian Castillo <fabianboc960@hotmail.com>
2022-08-14 19:54:41 -0500

    Interfaz Heroe: creacion de cards y flexlayout breakpoints

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -10,6 +11,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   exports: [
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
