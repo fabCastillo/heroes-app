875a328042afbcee599a1e9cd0adb7505052343c
Fabian Castillo <fabianboc960@hotmail.com>
2022-08-18 22:34:03 -0500

    Crear servicio de guardar Heroe; crear modulo y formulario de agregar Heroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-agregar',
@@ -8,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  public publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ]
+
+  public heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: ''
+  }
+  constructor(
+    private heroesService: HeroesService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public guardar(): void {
+    if (this.heroe.superhero.trim().length === 0) {
+      return;
+    }
+    this.heroesService.agregarHeroe( this.heroe )
+      .subscribe( resp => console.log('Respuesta:', resp) )
   }
 
 }
