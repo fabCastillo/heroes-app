8d96d5257250fc11830b2b30bc23dc4701a4e9ba
Fabian Castillo <fabianboc960@hotmail.com>
2022-08-23 21:40:08 -0500

    Mostrar información del usuario Activo

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Auth } from '../interfaces/auth.interface';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +10,18 @@ import { Observable } from 'rxjs';
 export class AuthService {
 
   private baseUrl: string = environment.baseUrl;
+  private _auth: Auth | undefined;
+
+  get auth(): Auth {
+    return { ...this._auth! };
+  }
 
   constructor( private http: HttpClient) { }
 
   public login(): Observable<Auth> {
-    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`);
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`)
+            .pipe(
+              tap( auth => this._auth = auth )
+            )
   }
 }
